8d0c1d446a100185e806d04bf5525d8e8f3030df
prosoftwaredevelopment <softwaredeveloper1234567890@gmail.com>
2020-02-01 12:35:10 +0530

    lecture 6

--- myfirstapp/urls.py
+++ myfirstapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('intro/<str:name>/<int:age>',views.intro,name="intro"),
     path('myfirstpage',views.myfirstpage,name='myfirstpage'),
     path('mysecondpage',views.mysecondpage,name='mysecondpage'),
+    path('mythirdpage',views.mythirdpage,name='mythirdpage')
+
 
 
 
--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -23,3 +23,20 @@ def myfirstpage(request):
 def mysecondpage(request):
     return render(request,'second.html')
 
+def mythirdpage(request):
+    var = "hello world"
+    greeting = "hey how are you"
+    fruits = ["apple","mango","banana"]
+    num1 , num2 = 10 , 7
+    ans = num1 > num2
+    #print(ans)
+    mydictionary = {
+        "var" : var,
+        "msg" : greeting,
+        "myfruits" : fruits,
+        "num1" : num1,
+        "num2" : num2,
+        "ans" : ans
+    }
+    return render(request,'third.html',context=mydictionary)
+
