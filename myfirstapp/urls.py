b3c98a006c2ad8de99397879913c0bb2079c42fb
prosoftwaredevelopment <softwaredeveloper1234567890@gmail.com>
2020-02-05 01:48:42 +0530

    getting started with images

--- myfirstapp/urls.py
+++ myfirstapp/urls.py
@@ -8,7 +8,13 @@ urlpatterns = [
     path('intro/<str:name>/<int:age>',views.intro,name="intro"),
     path('myfirstpage',views.myfirstpage,name='myfirstpage'),
     path('mysecondpage',views.mysecondpage,name='mysecondpage'),
-    path('mythirdpage',views.mythirdpage,name='mythirdpage')
+    path('mythirdpage',views.mythirdpage,name='mythirdpage'),
+    path('myimagepage',views.myimagepage,name='myimagepage'),
+    path('myimagepage2',views.myimagepage2,name='myimagepage2'),
+    path('myimagepage3',views.myimagepage3,name='myimagepage4'),
+    path('myimagepage4',views.myimagepage4,name='myimagepage4'),
+    path('myimagepage5/<str:imagename>',views.myimagepage5,name='myimagepage5'),
+
 
 
 
--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -40,3 +40,28 @@ def mythirdpage(request):
     }
     return render(request,'third.html',context=mydictionary)
 
+def myimagepage(request):
+    return render(request,'imagepage.html')
+
+
+def myimagepage2(request):
+    return render(request,'imagepage2.html')
+
+def myimagepage3(request):
+    return render(request,'imagepage3.html')
+
+def myimagepage4(request):
+    return render(request,'imagepage4.html')
+
+def myimagepage5(request,imagename):
+    myimagename = str(imagename)
+    myimagename = myimagename.lower()
+    print(myimagename)
+    if myimagename == "django":
+        var=True
+    elif myimagename == "python":
+        var=False
+    mydictionary ={
+        "var":var
+    }
+    return render(request,'imagepage5.html',context=mydictionary)
